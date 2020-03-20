4a708e73a2ba72e0c8341c120c81c69781e11360
Vladimir Tolmachev <vladimir.tolmachev@oneclickdev.com>
2020-03-20 09:24:56 +0300

    changes

---
+++ Clients/ShopMe.Client/Views/Glyphs.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopMe.Client.Views
+{
+    // https://andreinitescu.github.io/IconFont2Code/
+    // https://materialdesignicons.com/
+    // https://montemagno.com/using-font-icons-in-xamarin-forms-goodbye-images-hello-fonts/
+    // https://dev.to/codingcoach/using-font-awesome-in-xamarinforms--3mh
+    public static class Glyphs
+    {
+        public const string Pencil = "\U000f03eb";
+    }
+}
